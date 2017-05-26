05addb7a10fc117f69b0c4890acbf26ed192531c
Sammy Barasa <barsamms@gmail.com>
2017-05-26 14:14:28 +0300

    added migration classes Added new database credentials

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=crossover',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=clinic',
+    'username' => 'clinic',
+    'password' => 'clinic',
     'charset' => 'utf8',
 ];
