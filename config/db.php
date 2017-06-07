b4daddf9d473b3627531433f75bb911528ead906
Sammy Barasa <sammy.barasa@pristone.net>
2017-06-07 10:29:14 +0300

    added migration scripts added data seeder script

--- config/db.php
+++ config/db.php
@@ -1,9 +1,15 @@
 <?php
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=crossover',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;port=3307;dbname=clinic', // MySQL, MariaDB
+    //'dsn' => 'sqlite:/path/to/database/file', // SQLite
+    //'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase', // PostgreSQL
+    //'dsn' => 'cubrid:dbname=demodb;host=localhost;port=33000', // CUBRID
+    //'dsn' => 'sqlsrv:Server=localhost;Database=mydatabase', // MS SQL Server, sqlsrv driver
+    //'dsn' => 'dblib:host=localhost;dbname=mydatabase', // MS SQL Server, dblib driver
+    //'dsn' => 'mssql:host=localhost;dbname=mydatabase', // MS SQL Server, mssql driver
+    //'dsn' => 'oci:dbname=//localhost:1521/mydatabase', // Oracle
+    'username' => 'clinic',
+    'password' => 'clinic',
     'charset' => 'utf8',
 ];
